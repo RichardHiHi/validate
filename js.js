badab57be9a8e87be6118dfe7f03e0161133d61e
thanh <thanhtrong.nguyen1602@gmail.com>
2020-10-18 17:56:45 +0700

    finish validate project

--- js.js
+++ js.js
@@ -1,4 +1,13 @@
 function Validator(options) {
+  function getParent(element, selector) {
+    while (element.parentElement) {
+      if (element.parentElement.matches(selector)) {
+        return element.parentElement;
+      }
+      element = element.parentElement;
+    }
+  }
+  var selectorRule = {};
   var formElement = document.querySelector(options.form);
 
   //nếu nhập sai thi bôi đỏ còn đúng thì xóa đi
@@ -7,30 +16,68 @@ function Validator(options) {
     var errorElement = inputElement.parentElement.querySelector(
       options.formMessage
     );
-    var parentElement = inputElement.parentElement;
-    var errorMess = rule.test(inputElement.value);
+
+    console.log(errorElement);
+    console.log(getParent(inputElement, options.formGroup));
+    var errorMess;
+    rules = selectorRule[rule.select];
+    for (var i = 0; i < rules.length; i++) {
+      errorMess = rules[i](inputElement.value);
+      if (errorMess) {
+        break;
+      }
+    }
     if (errorMess) {
       errorElement.textContent = errorMess;
-      parentElement.classList.add("invalid");
+      getParent(inputElement, options.formGroup).classList.add("invalid");
     } else {
       errorElement.textContent = "";
-      parentElement.classList.remove("invalid");
+      getParent(inputElement, options.formGroup).classList.remove("invalid");
     }
+    return !errorMess;
   };
 
   if (formElement) {
-    options.rule.forEach((rule) => {
-      var inputElement = formElement.querySelector(rule.select);
+    formElement.onsubmit = function (e) {
+      var isFormValid = true;
+      e.preventDefault();
+      options.rule.forEach(function (rule) {
+        var inputElement = formElement.querySelector(rule.select);
+        isVaild = validate(inputElement, rule);
+        if (!isVaild) {
+          isFormValid = false;
+        }
+      });
+      console.log(isFormValid);
+      if (isFormValid) {
+        if (typeof options.onSubmit === "function") {
+          var data = formElement.querySelectorAll("[name]");
+          var formValue = Array.from(data).reduce(function (values, input) {
+            values[input.name] = input.value;
+            return values;
+          }, {});
+          options.onSubmit(formValue);
+        }
+      }
+    };
 
+    options.rule.forEach((rule) => {
+      if (Array.isArray(selectorRule[rule.select])) {
+        selectorRule[rule.select].push(rule.test);
+      } else {
+        selectorRule[rule.select] = [rule.test];
+      }
+      var inputElement = formElement.querySelector(rule.select);
       if (inputElement) {
         inputElement.onblur = function () {
           validate(inputElement, rule);
         };
         inputElement.oninput = function () {
-          var errorElement = inputElement.parentElement.querySelector(
-            options.formMessage
-          );
-          var parentElement = inputElement.parentElement;
+          var errorElement = getParent(
+            inputElement,
+            options.formGroup
+          ).querySelector(options.formMessage);
+          var parentElement = getParent(inputElement, options.formGroup);
           errorElement.innertext = "";
           parentElement.classList.remove("invalid");
         };
@@ -44,23 +91,45 @@ function Validator(options) {
 //1,khi có lỗi = trả về 1 mess erro
 //2.khi không có lỗi thì không trả về cái gì cả
 
-Validator.isRequired = function (selector) {
+Validator.isRequired = function (selector, message) {
   return {
     select: selector,
     test: function (value) {
-      return value ? undefined : "vui long nhap lai";
+      return value ? undefined : message || "vui long nhap lai";
     },
   };
 };
 
-Validator.isEmail = function (selector) {
+Validator.isEmail = function (selector, message) {
   return {
     select: selector,
     test: function (value) {
       var check = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
         value
       );
-      return check ? undefined : "vui long nhap lai";
+      return check ? undefined : message || "vui long nhap lai";
+    },
+  };
+};
+
+Validator.minlength = function (selector, min, message) {
+  return {
+    select: selector,
+    test: function (value) {
+      return value.length >= min
+        ? undefined
+        : message || `vui lòng nhập hơn ${min} kí tự`;
+    },
+  };
+};
+
+Validator.isConfirmPassword = function (selector, getValueConfirm, message) {
+  return {
+    select: selector,
+    test: function (value) {
+      return value === getValueConfirm()
+        ? undefined
+        : message || `mat khau bam nhap khong chinh xac`;
     },
   };
 };
